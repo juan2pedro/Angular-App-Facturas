ac243d4d27509a79a5f46dc7d8fb2939e99f23bc
Juan Pedro Mancera Taboada <juanpedro.mancera.contractor@bbva.com>
2025-01-22 12:41:45 +0100

    Añadido un componente y importado en app-componet
    - creado un contador

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CounterComponent } from "./counter/counter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title:string = 'Hola mundo Angular!';
-    // users:string[]= ['Pepe', 'María', 'Juan', 'Andres'];
-   users:string[]= [];
+    users:string[]= ['Pepe', 'María', 'Juan', 'Andres'];
+  // users:string[]= [];
   visible:boolean = false;
   setVisible():void{
     this.visible = this.visible? false : true;
