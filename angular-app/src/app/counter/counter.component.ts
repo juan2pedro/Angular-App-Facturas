750b1d01a691eff1ff6da5262ca64f077daa6db2
Juan Pedro Mancera Taboada <juanpedro.mancera.contractor@bbva.com>
2025-01-24 14:51:46 +0100

    - Creando app invoice

--- angular-app/src/app/counter/counter.component.ts
+++ angular-app/src/app/counter/counter.component.ts
@@ -9,7 +9,9 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class CounterComponent implements OnInit{
   @Input() title!:string;
   @Output() counterEmmit: EventEmitter<number> = new EventEmitter();
+
   counter: number = 0;
+
   ngOnInit(): void {
     if (typeof window !== 'undefined' && typeof sessionStorage !== 'undefined') {
       this.counter = parseInt(sessionStorage.getItem('counter')!) || 0;
